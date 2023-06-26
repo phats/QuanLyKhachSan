0f202bda8e4034d0f9b769b3116c286ae48df29b
unknown <bphm82@gmail.com>
2023-06-26 22:01:02 +0700

    service and minibar

--- routes/ticket.r.js
+++ routes/ticket.r.js
@@ -2,4 +2,7 @@ const app = require('express')
 const router = app.Router()
 const ticketC = require('../controllers/ticket.c')
 router.get('/ticket', ticketC.allTicket);
+router.post('/ticket/service', ticketC.addServiceToTicket)
+router.post('/ticket/minibar', ticketC.addMinibarToTicket)
+
 module.exports = router;
